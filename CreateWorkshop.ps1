cb3798bd99353c557854252192c94e02f5499ba5
Luca Bolognese <lucabol@microsoft.com>
2019-09-19 13:23:46 +0200

    First checkin.

---
+++ CreateWorkshop.ps1
@@ -0,0 +1,52 @@
+[CmdletBinding()]
+param()
+
+Import-Module .\Az.LabServices.psm1 -Force
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+$rgName     = 'WorkshopRG'
+$rgLocation = 'West Europe'
+$labName    = 'Advancing Differenciation'
+$laName     = 'Workshops'
+$imgName    = 'Windows 10 Pro, Version 1903'
+$maxUsers   = 2
+$usageQuota = 30
+$usageAMode = 'Restricted'
+$shPsswd    = $false
+$size       = 'Small'
+$title      = 'Advancing Differentiation Workshop'
+$descr      = 'Bringing it to the 21st Century'
+$userName   = 'test0000'
+$password   = 'Test00000000'
+$linuxRdp   = $true
+
+if(-not (Get-AzResourceGroup -ResourceGroupName $rgName -EA SilentlyContinue)) {
+    New-AzResourceGroup -ResourceGroupName $rgName -Location $rgLocation | Out-null
+    Write-Host "$rgname resource group didn't exist. Created it."
+}
+
+$la  = New-AzLabAccount -ResourceGroupName $rgName -LabAccountName $laName
+Write-Host "$laName lab account created or found."
+
+$lab = $la | Get-AzLab -LabName $labName
+
+if($lab) {
+    $lab = $la `
+        | New-AzLab -LabName $LabName -MaxUsers $maxUsers -UsageQuotaInHours $usageQuota -UserAccessMode $usageAMode -SharedPasswordEnabled:$shPsswd `
+        | Publish-AzLab
+    Write-Host "$LabName lab already exist. Republished."
+} else {
+    $img = $la | Get-AzLabAccountGalleryImage | Where-Object {$_.name -like $imgName}
+    if(-not $img -or $img.Count -ne 1) {Write-Error "$imgName pattern doesn't match just one image."}
+    Write-Host "Image $imgName found."
+    
+    $lab = $la `
+        | New-AzLab -LabName $LabName -MaxUsers $maxUsers -UsageQuotaInHours $usageQuota -UserAccessMode $usageAMode -SharedPasswordEnabled:$shPsswd `
+        | New-AzLabTemplateVM -Image $img -Size $size -Title $title -Description $descr -UserName $userName -Password $password -LinuxRdpEnabled:$linuxRdp `
+        | Publish-AzLab
+    Write-Host "$LabName lab doesn't exist. Created it."
+}
+
+Remove-Module Az.LabServices -Force
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# AzureLabServicesWorkshop
-Script to run before the workshop. See Readme.
+# Here is how to create the workshop
+
+1. Open the Azure portal in your browser: https://ms.portal.azure.com/#home
+2. Create an Azure Cloud Shell by clicking on the icon below. ![Icon to click](Console.jpg)
+3. Switch to Powershell mode by clicking on the icon below (if not already in Powershell mode). ![Icon to click](Console.jpg)
+4. Download the 
+
